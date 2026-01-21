a2f81921dcbd0e43b2984c0a168d9f44a8937876
Bu211050 <desinghmariya@gmail.com>
2026-01-21 23:27:57 +0530

    Fix: Corrected asset prefix for images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     basePath: isProd ? '/MD_Portfolio' : '',
-    assetPrefix: isProd ? '/MD_Portfolio/' : '',
+    assetPrefix: isProd ? '/MD_Portfolio' : '',
     images: {
         unoptimized: true,
     },
